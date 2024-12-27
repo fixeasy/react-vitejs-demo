2f1f664d45bef8ab5f5bca5e7d61dc67c2797042
Mehdi KADAR <ext.mehdi.kadar@sncf.fr>
2024-12-27 10:49:56 +0100

    feat: link to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: "react-vitejs-demo",
     server: {
         host: true, // Nécessaire pour le mappage des ports dans Docker
         port: 5173, // Port sur lequel Vite écoute
